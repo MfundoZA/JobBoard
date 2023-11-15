e0fb8ca7aae584b3df4095b42091a0eabc2efca2
Mfundo <jordantenza@gmail.com>
2023-11-15 14:17:46 +0200

    Created User model

---
+++ JobBoard/Models/User.cs
@@ -0,0 +1,29 @@
+﻿namespace JobBoard.API.Models
+{
+    public class User
+    {
+        public int Id { get; private set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public IList<Resume> Resumes { get; set; }
+        public string CityBased { get; set; }
+        public IList<string> ProfileLinks { get; set; }
+
+        public User(int id, string firstName, string lastName, string email, string password,
+            string phoneNumber, IList<Resume> resumes, string cityBased, IList<string> profileLinks)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Password = password;
+            PhoneNumber = phoneNumber;
+            Resumes = resumes;
+            CityBased = cityBased;
+            ProfileLinks = profileLinks;
+        }
+    }
+}

6bde09a28121e3bcdf14dead0a811b89aedff4c7
Mfundo <jordantenza@gmail.com>
2023-11-17 10:37:53 +0200

    Implemented Skill class

--- JobBoard/Models/Skill.cs
+++ JobBoard/Models/Skill.cs
@@ -2,5 +2,15 @@
 {
     public class Skill
     {
+        public int Id { get; private set; }
+        public string SkillName { get; set; }
+        public string SkillCatergory { get; set; }
+
+        public Skill(int id, string skillName, string skillCatergory)
+        {
+            Id = id;
+            SkillName = skillName;
+            SkillCatergory = skillCatergory;
+        }
     }
 }

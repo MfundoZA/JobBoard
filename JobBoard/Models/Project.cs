9b80884b93fe39ef125263ed5b183721e5bf5134
Mfundo <65462513+MfundoZA@users.noreply.github.com>
2023-11-19 21:33:17 +0200

    Implemented Project class

--- JobBoard/Models/Project.cs
+++ JobBoard/Models/Project.cs
@@ -2,5 +2,21 @@
 {
     public class Project
     {
+        public int Id { get; set; }
+        public string ProjectName { get; set; }
+        public string ProjectDescription { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public List<String> ProjectBulletPoints { get; set; }
+
+        public Project(int id, string ProjectName, string? ProjectDescription, DateTime? startDate, DateTime? EndDate, List<string>? projectBulletPoints)
+        {
+            Id = id;
+            ProjectName = projectName;
+            ProjectDescription = projectDescription;
+            StartDate = startDate;
+            EndDate = endDate;
+            ProjectBulletPoints = projectBulletPoints;
+        }
     }
 }

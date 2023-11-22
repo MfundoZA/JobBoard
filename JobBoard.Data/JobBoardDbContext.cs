bef35eb966abb5d0ab5b6023b0bd593fef30ddf0
Mfundo <jordantenza@gmail.com>
2023-11-22 13:38:53 +0200

    Declared Users and Resumes DbSets

---
+++ JobBoard.Data/JobBoardDbContext.cs
@@ -0,0 +1,16 @@
+﻿using JobBoard.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobBoard.Data
+{
+    public class JobBoardDbContext : DbContext
+    {
+        DbSet<User> Users { get; set; } = null!;
+        DbSet<Resume> Resumes { get; set; } = null!;
+    }
+}

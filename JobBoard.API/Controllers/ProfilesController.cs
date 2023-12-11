d0dbb11fd5557c1fc1d247b1ce77c320a9cc8fec
Mfundo <jordantenza@gmail.com>
2023-12-11 21:33:39 +0200

    Created ProfilesController

---
+++ JobBoard.API/Controllers/ProfilesController.cs
@@ -0,0 +1,122 @@
+﻿using JobBoard.Data.Models;
+using JobBoard.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace JobBoard.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfilesController : ControllerBase
+    {
+        private readonly JobBoardDbContext _context;
+
+        public ProfilesController(JobBoardDbContext context)
+        {
+                _context = context;
+        }
+
+        // GET: api/Profiles
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Profile>>> GetProfiles()
+        {
+            if (_context.Profiles == null)
+            {
+                return NotFound();
+            }
+
+            return await _context.Profiles.ToListAsync();
+        }
+
+        // GET: api/Profiles/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Profile>> GetProfile(int id)
+        {
+            if (_context.Profiles == null)
+            {
+                return NotFound();
+            }
+            var profile = await _context.Profiles.FindAsync(id);
+
+            if (profile == null)
+            {
+                return NotFound();
+            }
+
+            return profile;
+        }
+
+        // PUT: api/Profiles/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProfile(int id, Profile profile)
+        {
+            if (id != profile.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(profile).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProfileExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Products
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Profile>> PostProfile(Profile profile)
+        {
+            if (_context.Profiles == null)
+            {
+                return Problem("Entity set 'JobBoardDbContext.Profile' is null.");
+            }
+            _context.Profiles.Add(profile);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProfile", new { id = profile.Id }, profile);
+        }
+
+        // DELETE: api/Profiles/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProfile(int id)
+        {
+            if (_context.Profiles == null)
+            {
+                return NotFound();
+            }
+            var profile = await _context.Profiles.FindAsync(id);
+            if (profile == null)
+            {
+                return NotFound();
+            }
+
+            _context.Profiles.Remove(profile);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProfileExists(int id)
+        {
+            return (_context.Profiles?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}

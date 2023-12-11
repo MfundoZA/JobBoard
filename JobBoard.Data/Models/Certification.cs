206fac9587bf6fdc9dc4440a260a6818b16f3451
Mfundo <jordantenza@gmail.com>
2023-12-11 13:06:59 +0200

    Created Certification class

---
+++ JobBoard.Data/Models/Certification.cs
@@ -0,0 +1,19 @@
+﻿using JobBoard.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobBoard.Data.Models
+{
+    public class Certification
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Issuer { get; set; } = null!;
+        public DateTime DateIssued { get; set; }
+        public DateTime? DateExpires { get; set; }
+        public List<Profile>? Profiles { get; set; }
+    }
+}

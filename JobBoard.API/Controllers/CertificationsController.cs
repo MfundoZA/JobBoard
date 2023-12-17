4b8416109d1a4a3d82d60641877ce48931c355ca
Mfundo <jordantenza@gmail.com>
2023-12-17 09:37:25 +0200

    Created certifications and skills controllers

---
+++ JobBoard.API/Controllers/CertificationsController.cs
@@ -0,0 +1,122 @@
+﻿using JobBoard.Data.Models;
+using JobBoard.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace JobBoard.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CertificationsController : ControllerBase
+    {
+        private readonly JobBoardDbContext _context;
+
+        public CertificationsController(JobBoardDbContext context)
+        {
+                _context = context;
+        }
+
+        // GET: api/Certifications
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Certification>>> GetCertificates()
+        {
+            if (_context.Certifications == null)
+            {
+                return NotFound();
+            }
+
+            return await _context.Certifications.ToListAsync();
+        }
+
+        // GET: api/Certifications/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Certification>> GetCertification(int id)
+        {
+            if (_context.Certifications == null)
+            {
+                return NotFound();
+            }
+            var certification = await _context.Certifications.FindAsync(id);
+
+            if (certification == null)
+            {
+                return NotFound();
+            }
+
+            return certification;
+        }
+
+        // PUT: api/Certifications/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCertification(int id, Certification certification)
+        {
+            if (id != certification.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(certification).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CertificationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Certifications
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Certification>> PostCertifications(Certification certification)
+        {
+            if (_context.Certifications == null)
+            {
+                return Problem("Entity set 'JobBoardDbContext.Certification' is null.");
+            }
+            _context.Certifications.Add(certification);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCertification", new { id = certification.Id }, certification);
+        }
+
+        // DELETE: api/Certifications/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCertification(int id)
+        {
+            if (_context.Certifications == null)
+            {
+                return NotFound();
+            }
+            var certification = await _context.Certifications.FindAsync(id);
+            if (certification == null)
+            {
+                return NotFound();
+            }
+
+            _context.Certifications.Remove(certification);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CertificationExists(int id)
+        {
+            return (_context.Certifications?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ JobBoard.API/Controllers/SkillsController.cs
@@ -0,0 +1,122 @@
+﻿using JobBoard.Data.Models;
+using JobBoard.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace JobBoard.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SkillsController : ControllerBase
+    {
+        private readonly JobBoardDbContext _context;
+
+        public SkillsController(JobBoardDbContext context)
+        {
+                _context = context;
+        }
+
+        // GET: api/Skills
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Skill>>> GetSkills()
+        {
+            if (_context.Skills == null)
+            {
+                return NotFound();
+            }
+
+            return await _context.Skills.ToListAsync();
+        }
+
+        // GET: api/Skills/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Skill>> GetSkill(int id)
+        {
+            if (_context.Skills == null)
+            {
+                return NotFound();
+            }
+            var skill = await _context.Skills.FindAsync(id);
+
+            if (skill == null)
+            {
+                return NotFound();
+            }
+
+            return skill;
+        }
+
+        // PUT: api/Skills/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSkill(int id, Skill skill)
+        {
+            if (id != skill.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(skill).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SkillExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Skills
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Skill>> PostSkill(Skill skill)
+        {
+            if (_context.Skills == null)
+            {
+                return Problem("Entity set 'JobBoardDbContext.Skill' is null.");
+            }
+            _context.Skills.Add(skill);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSkill", new { id = skill.Id }, skill);
+        }
+
+        // DELETE: api/Skills/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSkill(int id)
+        {
+            if (_context.Skills == null)
+            {
+                return NotFound();
+            }
+            var skill = await _context.Skills.FindAsync(id);
+            if (skill == null)
+            {
+                return NotFound();
+            }
+
+            _context.Skills.Remove(skill);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SkillExists(int id)
+        {
+            return (_context.Skills?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}

280d3e9d1ff8ccec1ed40d1a3c60b60dc3f84608
Mfundo <jordantenza@gmail.com>
2023-12-17 09:35:32 +0200

    Updated database to support job postings and applications

---
+++ JobBoard.Data/Migrations/20231217064326_AddedSupportForOpenings.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobBoard.Data.Migrations
+{
+    public partial class AddedSupportForOpenings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CertificationProfiles_Certifications_CertificationsId",
+                table: "CertificationProfiles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProfileSkills_Skills_SkillsId",
+                table: "ProfileSkills");
+
+            migrationBuilder.DropTable(
+                name: "Milestones");
+
+            migrationBuilder.DropTable(
+                name: "Projects");
+
+            migrationBuilder.DropTable(
+                name: "Skills");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Certifications",
+                table: "Certifications");
+
+            migrationBuilder.RenameTable(
+                name: "CertificationProfiles",
+                newName: "ProfileCertifications");
+
+            migrationBuilder.AddColumn<int>(
+                name: "OpeningId",
+                table: "Profiles",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Certifications",
+                table: "Certifications",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CityBased = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CompanySize = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Milestones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MilestoneType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CompanyName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ProfileId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Milestones", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Milestones_Profiles_ProfileId",
+                        column: x => x.ProfileId,
+                        principalTable: "Profiles",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProjectName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProjectDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ProfileId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Projects_Profiles_ProfileId",
+                        column: x => x.ProfileId,
+                        principalTable: "Profiles",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Skills",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SkillTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SkillCatergory = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Skills", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Openings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CompanyId = table.Column<int>(type: "int", nullable: false),
+                    DatePosted = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ClosingDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    JobType = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Responsibilities = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Requirements = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DesiredExperience = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Openings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Openings_Companies_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Companies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Profiles_OpeningId",
+                table: "Profiles",
+                column: "OpeningId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Milestones_ProfileId",
+                table: "Milestones",
+                column: "ProfileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Openings_CompanyId",
+                table: "Openings",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_ProfileId",
+                table: "Projects",
+                column: "ProfileId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProfileCertifications_Certifications_CertificationsId",
+                table: "ProfileCertifications",
+                column: "CertificationsId",
+                principalTable: "Certifications",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Profiles_Openings_OpeningId",
+                table: "Profiles",
+                column: "OpeningId",
+                principalTable: "Openings",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProfileSkills_Skills_SkillsId",
+                table: "ProfileSkills",
+                column: "SkillsId",
+                principalTable: "Skills",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CertificationProfile_Certifications_CertificationsId",
+                table: "CertificationProfile");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Profiles_Openings_OpeningId",
+                table: "Profiles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProfileSkill_Skills_SkillsId",
+                table: "ProfileSkill");
+
+            migrationBuilder.DropTable(
+                name: "Milestones");
+
+            migrationBuilder.DropTable(
+                name: "Openings");
+
+            migrationBuilder.DropTable(
+                name: "Projects");
+
+            migrationBuilder.DropTable(
+                name: "Skills");
+
+            migrationBuilder.DropTable(
+                name: "Companies");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Profiles_OpeningId",
+                table: "Profiles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Certifications",
+                table: "Certifications");
+
+            migrationBuilder.DropColumn(
+                name: "OpeningId",
+                table: "Profiles");
+
+            migrationBuilder.RenameTable(
+                name: "Certifications",
+                newName: "Certification");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Certification",
+                table: "Certification",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Milestone",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CompanyName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    MilestoneType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProfileId = table.Column<int>(type: "int", nullable: true),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Milestone", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Milestone_Profiles_ProfileId",
+                        column: x => x.ProfileId,
+                        principalTable: "Profiles",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Project",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ProfileId = table.Column<int>(type: "int", nullable: true),
+                    ProjectDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProjectName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Project", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Project_Profiles_ProfileId",
+                        column: x => x.ProfileId,
+                        principalTable: "Profiles",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Skill",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SkillCatergory = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SkillTitle = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Skill", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Milestone_ProfileId",
+                table: "Milestone",
+                column: "ProfileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Project_ProfileId",
+                table: "Project",
+                column: "ProfileId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CertificationProfile_Certification_CertificationsId",
+                table: "CertificationProfile",
+                column: "CertificationsId",
+                principalTable: "Certification",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProfileSkill_Skill_SkillsId",
+                table: "ProfileSkill",
+                column: "SkillsId",
+                principalTable: "Skill",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}

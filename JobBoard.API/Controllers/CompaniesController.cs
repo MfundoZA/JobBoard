0a00cfa00ba635d56c80db8479ccf55fee1f3d6a
MfundoZA <jordantenza@gmail.com>
2023-12-18 20:42:03 +0200

    Created Companies and Openings controller

---
+++ JobBoard.API/Controllers/CompaniesController.cs
@@ -0,0 +1,123 @@
+﻿using JobBoard.Data.Models;
+using JobBoard.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace JobBoard.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompaniesController : ControllerBase
+    {
+        private readonly JobBoardDbContext _context;
+
+        public CompaniesController(JobBoardDbContext context)
+        {
+                _context = context;
+        }
+
+        // GET: api/Users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Company>>> GetCompanies()
+        {
+            if (_context.Companies == null)
+            {
+                return NotFound();
+            }
+
+            return await _context.Companies.ToListAsync();
+        }
+
+        // GET: api/Companies/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Company>> GetCompany(int id)
+        {
+            if (_context.Companies == null)
+            {
+                return NotFound();
+            }
+            var company = await _context.Companies.FindAsync(id);
+
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            return company;
+        }
+
+        // PUT: api/Companies/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCompany(int id, Company company)
+        {
+            if (id != company.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(company).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CompanyExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Users
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<User>> PostCompany(Company company)
+        {
+            if (_context.Companies == null)
+            {
+                return Problem("Entity set 'JobBoardDbContext.Company' is null.");
+            }
+            _context.Companies.Add(company);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCompany", new { id = company.Id }, company);
+        }
+
+        // DELETE: api/Users/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCompany(int id)
+        {
+            if (_context.Companies == null)
+            {
+                return NotFound();
+            }
+            var company = await _context.Companies.FindAsync(id);
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            _context.Companies.Remove(company);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CompanyExists(int id)
+        {
+            return (_context.Companies?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ JobBoard.API/Controllers/OpeningsController.cs
@@ -0,0 +1,127 @@
+﻿using JobBoard.Data.Models;
+using JobBoard.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace JobBoard.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OpeningsController : ControllerBase
+    {
+        private readonly JobBoardDbContext _context;
+
+        public OpeningsController(JobBoardDbContext context)
+        {
+                _context = context;
+        }
+
+        // GET: api/Openings
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Opening>>> GetOpenings()
+        {
+            if (_context.Openings == null)
+            {
+                return NotFound();
+            }
+
+            return await _context.Openings.ToListAsync();
+        }
+
+        // GET: api/Openings/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Opening>> GetOpening(int id)
+        {
+            if (_context.Openings == null)
+            {
+                return NotFound();
+            }
+            var opening = await _context.Openings.FindAsync(id);
+
+            if (opening == null)
+            {
+                return NotFound();
+            }
+
+            return opening;
+        }
+
+        // PUT: api/Openings/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser(int id, Opening opening)
+        {
+            if (id != opening.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(opening).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OpeningExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Openings
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Opening>> PostOpening(Opening opening)
+        {
+            if (_context.Openings == null)
+            {
+                return Problem("Entity set 'JobBoardDbContext.Opening' is null.");
+            }
+            _context.Openings.Add(opening);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetOpening", new { id = opening.Id }, opening);
+        }
+
+        // DELETE: api/Openings/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOpening(int id)
+        {
+            if (_context.Openings == null)
+            {
+                return NotFound();
+            }
+            var opening = await _context.Openings.FindAsync(id);
+            if (opening == null)
+            {
+                return NotFound();
+            }
+
+            _context.Openings.Remove(opening);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool OpeningExists(int id)
+        {
+            return (_context.Openings?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+
+        private bool ApplicantExists(int openingId, int applicantId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

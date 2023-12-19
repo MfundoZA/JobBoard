f1f1c5c87a2c645eab46fc0250622cd2a4bb0ef5
MfundoZA <jordantenza@gmail.com>
2023-12-19 15:37:59 +0200

    Implementing ApplicantsController

---
+++ JobBoard.API/Controllers/ApplicantsController.cs
@@ -0,0 +1,39 @@
+﻿using JobBoard.Data.Models;
+using JobBoard.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace JobBoard.API.Controllers
+{
+    [Route("api/Applicants")]
+    [ApiController]
+    public class ApplicantsController : ControllerBase
+    {
+        private readonly JobBoardDbContext _context;
+        public IHttpContextAccessor? HttpContextAccessor { get; set; }
+
+        public ApplicantsController(JobBoardDbContext context, IHttpContextAccessor httpContextAccessor)
+        {
+            _context = context;
+            HttpContextAccessor = httpContextAccessor;
+        }
+
+        // GET: api/Applicants
+        // Will return the current company's applicants
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProfileOpening>>> GetApplicants()
+        {
+            var currentCompanyId = HttpContextAccessor?.HttpContext?.Session.GetInt32("companyId");
+            var companyApplicants = _context.ProfileOpenings?.Where(e => e.Opening.Company.Id == currentCompanyId);
+
+            if (companyApplicants == null)
+            {
+                return NotFound();
+            }
+
+            return await companyApplicants.ToListAsync();
+        }
+    }
+}

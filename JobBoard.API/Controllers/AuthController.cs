5a5bc9f9789a2d63accc224d3d2edbf08f876b1f
MfundoZA <jordantenza@gmail.com>
2023-12-29 09:57:56 +0200

    Optimized using staments

--- JobBoard.API/Controllers/AuthController.cs
+++ JobBoard.API/Controllers/AuthController.cs
@@ -1,7 +1,9 @@
 ﻿using JobBoard.Data.Models;
 using JobBoard.Data;
+using JobBoard.Data.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 

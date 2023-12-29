ff1a65f21d4ca8013acffd285281265c0930642a
MfundoZA <jordantenza@gmail.com>
2023-12-29 09:54:49 +0200

    Removed nullable operator since EF ensures tables can be empty but never null

--- JobBoard.Data/JobBoardDbContext.cs
+++ JobBoard.Data/JobBoardDbContext.cs
@@ -12,22 +12,22 @@ namespace JobBoard.Data
     public class JobBoardDbContext : DbContext
     {
         // Entry tables
-        public DbSet<User>? Users { get; set; }
-        public DbSet<Profile>? Profiles { get; set; }
-        public DbSet<Milestone>? Milestones { get; set; }
-        public DbSet<Project>? Projects { get; set; }
-        public DbSet<Skill>? Skills { get; set; }
-        public DbSet<Certification>? Certifications { get; set; }
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<Profile> Profiles { get; set; } = null!;
+        public DbSet<Milestone> Milestones { get; set; } = null!;
+        public DbSet<Project> Projects { get; set; } = null!;
+        public DbSet<Skill> Skills { get; set; } = null!;
+        public DbSet<Certification> Certifications { get; set; } = null!;
 
-        public DbSet<Company>? Companies { get; set; }
-        public DbSet<Opening>? Openings { get; set; }
+        public DbSet<Company> Companies { get; set; } = null!;
+        public DbSet<Opening> Openings { get; set; } = null!;
 
         // Lookup tables
-        public DbSet<ProfileSkill>? ProfileSkills { get; set; }
-        public DbSet<ProfileCertification>? ProfileCertifications { get; set; }
-        public DbSet<ProfileOpening>? ProfileOpenings { get; set; }
+        public DbSet<ProfileSkill> ProfileSkills { get; set; } = null!;
+        public DbSet<ProfileCertification> ProfileCertifications { get; set; } = null!;
+        public DbSet<ProfileOpening> ProfileOpenings { get; set; } = null!;
 
-        public DbSet<Recruiter>? Recruiters { get; set; } 
+        public DbSet<Recruiter> Recruiters { get; set; } = null!;
 
         public JobBoardDbContext() { }
 
